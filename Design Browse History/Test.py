15a1c9928e8aedb8d81f70d905d2bea62aca2c1d
NotDiego7 <Lopez.d7@outlook.com>
2023-08-27 21:18:10 -0600

    Initiate

---
+++ Design Browse History/Test.py
@@ -0,0 +1,23 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+        self.previous = None
+
+"""
+You have a browser of one tab where you start on the homepage.
+You can visit another URL, get back in history a number of steps or move forward in history a number of steps
+"""
+#TODO: 
+#TODO: 
+
+def algorithm():
+    pass
+
+
+list1 = algorithm()
+
+
+while list1:
+    print(list1.val)
+    list1 = list1.next
