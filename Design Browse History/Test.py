0cbffc2d3f0d3f347a006ec4bc22845aa8d9fb67
NotDiego7 <Lopez.d7@outlook.com>
2023-08-28 14:23:03 -0600

    Functional Build

--- Design Browse History/Test.py
+++ Design Browse History/Test.py
@@ -8,16 +8,37 @@ class Node:
 You have a browser of one tab where you start on the homepage.
 You can visit another URL, get back in history a number of steps or move forward in history a number of steps
 """
-#TODO: 
-#TODO: 
+#NOTE: 
 
-def algorithm():
-    pass
+class BrowserHistory:
+
+    def __init__(self, homepage: str):
+        self.curr = Node(homepage)
+
+    def visit(self, url: str) -> None:
+        new_node = Node(url)
+        new_node.previous = self.curr
+        self.curr.next = new_node
+        self.curr = new_node
+        return None
 
 
-list1 = algorithm()
+    def back(self, steps: int) -> str:
+        while self.curr.previous and steps:
+            self.curr = self.curr.previous
+            steps -= 1
+        return self.curr.val
 
 
-while list1:
-    print(list1.val)
-    list1 = list1.next
+    def forward(self, steps: int) -> str:
+        while self.curr.next and steps:
+            self.curr = self.curr.next
+            steps -= 1
+        return self.curr.val
+
+
+# Your BrowserHistory object will be instantiated and called as such:
+# obj = BrowserHistory(homepage)
+# obj.visit(url)
+# param_2 = obj.back(steps)
+# param_3 = obj.forward(steps)
